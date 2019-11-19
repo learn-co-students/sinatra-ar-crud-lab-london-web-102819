e7a0a422f11dd11964c33f735c375a867fd12f1a
github username <github email address>
2019-11-19 16:30:43 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 
 require_relative '../../config/environment'
 
+
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -10,4 +11,50 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    
+    @article = Article.create(title: params["title"], content: params["content"])
+    
+    redirect "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+
+    erb :show
+  end
+
+  get '/articles' do
+    @articles = Article.all
+
+    erb :index
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+   
+    @article = Article.find(params[:id])
+    @article.update(title: params["title"], content: params["content"])
+
+    redirect "/articles/#{params[:id]}"
+  end
+
+  delete '/articles/:id' do
+    Article.destroy(params[:id])
+
+    redirect '/articles'
+  end
+
+
 end
+
